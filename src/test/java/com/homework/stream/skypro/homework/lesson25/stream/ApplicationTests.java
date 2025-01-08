461538c2c232e31dd1bbed660e57bd9f69e26a87
danie <daniel.isaes@mail.ru>
2025-01-08 15:41:28 +0300

    initialization

---
+++ src/test/java/com/homework/stream/skypro/homework/lesson25/stream/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.stream.skypro.homework.lesson25.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
